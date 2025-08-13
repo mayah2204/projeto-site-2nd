b0a5dc5289c4b9417e4314f76f873868d8a11a8e
mayah2204 <mayara.pelais@escola.pr.gov.br>
2025-08-13 20:53:23 -0300

    Create script.js aula8

---
+++ script.js
@@ -0,0 +1,9 @@
+window.srv = ScrollReveal ({reset: true });
+
+sr.reveal ({"#image" , duration: 1000}); 
+
+sr.reveal ({"#textoconteudo" , {
+     rotade: {x=0, y=100, z=0}
+
+       
+sr.reveal: ("#formulario" , {duration: 1500});
